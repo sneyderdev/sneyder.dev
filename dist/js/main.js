c954949de4868b2cb6ebd5c03224ffd57bf92ab9
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-05-18 15:52:11 -0500

    Changing menu's position and fixing overflows

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,9 @@
 $burgerBtn = document.getElementById('burger-btn');
 $menu = document.getElementById('menu');
+$body = document.getElementById('body');
 
 $burgerBtn.addEventListener('click', () => {
   $burgerBtn.classList.toggle('burger-btn--active');
   $menu.classList.toggle('menu--active');
+  $body.classList.toggle('overflow--hidden');
 });
