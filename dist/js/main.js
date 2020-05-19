955dfd0698281aaeb0ad668cec93eb8db755b931
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-05-19 18:07:43 -0500

    Organizing SASS structure, adding responsive (M/L) for index, fixing some HTML and CSS structure and improving the grid system

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,10 @@
+$blur = document.getElementById('blur');
 $burgerBtn = document.getElementById('burger-btn');
 $menu = document.getElementById('menu');
 $body = document.getElementById('body');
 
 $burgerBtn.addEventListener('click', () => {
+  $blur.classList.toggle('blur--active');
   $burgerBtn.classList.toggle('burger-btn--active');
   $menu.classList.toggle('menu--active');
   $body.classList.toggle('overflow--hidden');
