f867389a50b0293f75773218b36f96ababf28315
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-07-11 20:15:10 -0500

    :sparkles: Add animation when changing/minimizing browser's tab

--- dist/js/main.js
+++ dist/js/main.js
@@ -9,3 +9,33 @@ $burgerBtn.addEventListener('click', () => {
   $menu.classList.toggle('menu--active');
   $body.classList.toggle('overflow--hidden');
 });
+
+const $sleepScreen = document.querySelector('.sleep-screen');
+const $sleepText = document.querySelector('.sleep-screen__text');
+let sleepScreenPhrases = [
+  'Welcome back!',
+  'Hello again!',
+  "It's good to see you again!",
+  'Yo! 😄',
+];
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
+
+function handleVisibilityChange() {
+  const isVisible = document.visibilityState === 'visible';
+  let randomPhrase = Math.round(Math.random() * (sleepScreenPhrases.length - 1));
+
+    if (isVisible) {
+      $sleepText.innerText = sleepScreenPhrases[randomPhrase];
+      $sleepScreen.classList.add('sleep-screen--active');
+      $sleepText.classList.add('slideUp');
+      setTimeout(() => {
+        $sleepScreen.classList.remove('sleep-screen--active');
+        $sleepText.classList.remove('slideUp');
+      }, 2400);
+    } else {
+      $sleepText.innerText = '';
+      $sleepScreen.classList.remove('sleep-screen--active');
+      $sleepText.classList.remove('slideUp');
+    }
+}
