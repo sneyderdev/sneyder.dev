1a2c8b074776d70d47fa2575fe3a6c401621861d
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-12-02 22:43:54 -0500

    BUILD: Add ESLint and Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: red;
+`;
 
 const App = () => {
   return (
     <div>
-      <h1>sneyder.dev</h1>
+      <Title>sneyder.dev</Title>
     </div>
   );
 };
