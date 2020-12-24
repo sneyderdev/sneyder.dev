f98cac39a6d9193abfc050723d2fbd9f827b5f4a
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-12-24 18:55:59 -0500

    REFACTOR: Export Layout in components index and update import in RouteWrapper

--- src/routes/RouteWrapper.jsx
+++ src/routes/RouteWrapper.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Layout from '../components/Layout';
+
+import { Layout } from '../components';
 
 const RouteWrapper = ({ component: Component, hasArrow, isHome, ...rest }) => {
   return (
