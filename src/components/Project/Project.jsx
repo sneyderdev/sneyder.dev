fe04528f97a0267c5d5b86b5133f537129151b21
Sneyder Barreto <sneyderbarreto@gmail.com>
2021-01-05 22:42:23 -0500

    REFACTOR: Delete projectURL variable and destructure project's info

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -12,26 +12,24 @@ import ProjectOptions from './Project.styles';
 import { SocialMenuIcon } from '../../shared';
 
 const Project = ({ project }) => {
-  const projectURL = `/portfolio/${project.name
-    .replace(/(\.|\s)/, '-')
-    .toLowerCase()}`;
+  const { codeURL, previewURL, slug } = project;
 
   return (
     <Window project={project}>
       <ProjectOptions>
         <div>
-          <a href={project.codeURL} target='_blank' rel='noreferrer'>
+          <a href={codeURL} target='_blank' rel='noreferrer'>
             <span>Code</span>
             <SocialMenuIcon>
               <img src={linkIcon} alt='Link' />
               <img src={githubLogo} alt='GitHub Logo' />
             </SocialMenuIcon>
           </a>
-          <a href={project.previewURL} target='_blank' rel='noreferrer'>
+          <a href={previewURL} target='_blank' rel='noreferrer'>
             Live Preview
             <img src={arrowIcon} alt='Preview arrow' />
           </a>
-          <Link to={projectURL}>
+          <Link to={`portfolio/${slug}`}>
             Learn more... <img src={purpleArrowIcon} alt='Learn more arrow' />
           </Link>
         </div>
