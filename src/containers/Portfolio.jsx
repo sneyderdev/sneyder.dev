cfdd856b77fb9bd840a349ac1195affe83fdbab2
Sneyder Barreto <sneyderbarreto@gmail.com>
2021-01-05 22:34:18 -0500

    ADD: Fetch Projects data from Sanity and pass the state to the  Projects component

--- src/containers/Portfolio.jsx
+++ src/containers/Portfolio.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import initialState from '../initialState';
+import client from '../client';
 
 import { Projects, PinnedMessage } from '../components';
 
 import { TitleContainer, Title } from '../shared';
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    client
+      .fetch(
+        `*[_type == 'project']{
+          _id,
+          name,
+          codeUrl,
+          previewUrl,
+          'slug': slug.current,
+          'images': images[0].asset->url
+        }`
+      )
+      .then((data) => setProjects(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <TitleContainer center>
@@ -16,7 +35,7 @@ const Portfolio = () => {
 
       <PinnedMessage message={initialState.pinnedMessages.projects} />
 
-      <Projects projects={initialState.projects} />
+      <Projects projects={projects} />
     </>
   );
 };
