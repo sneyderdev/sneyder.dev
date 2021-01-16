5b063f0fa9113dbd3b9b18ffe8afda90df7c1091
Sneyder Barreto <sneyderbarreto@gmail.com>
2021-01-15 21:10:05 -0500

    ADD: Set default width for images inside ListItem

--- src/shared/components/ListItem.js
+++ src/shared/components/ListItem.js
@@ -5,6 +5,18 @@ const ListItem = styled.li`
   grid-template-columns: 15px 1fr;
   align-items: center;
   column-gap: 20px;
+
+  img {
+    width: 15px;
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: 20px 1fr;
+
+    img {
+      width: 20px;
+    }
+  }
 `;
 
 export default ListItem;
